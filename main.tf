1615ca419e0b1a1834a626628ab28cf8291d0a69
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2025-05-20 12:16:36 +0200

    feat!: add type definitions (#78)
    * feat!: small refactor
    BREAKING CHANGE: The data structure changed, causing a recreate on existing resources.
    
    * feat: increment module version
    
    * feat: make name required

--- main.tf
+++ main.tf
@@ -1,24 +1,36 @@
 # bastion host
 resource "azurerm_bastion_host" "bastion" {
-  name                = try(var.host.name, var.naming.bastion_host)
-  resource_group_name = coalesce(lookup(var.host, "resource_group", null), var.resource_group)
-  location            = coalesce(lookup(var.host, "location", null), var.location)
 
-  sku                       = try(var.host.sku, "Standard")
-  scale_units               = try(var.host.scale_units, 2)
-  copy_paste_enabled        = try(var.host.copy_paste_enabled, false)
-  file_copy_enabled         = try(var.host.file_copy_enabled, false)
-  tunneling_enabled         = try(var.host.tunneling_enabled, false)
-  ip_connect_enabled        = try(var.host.ip_connect_enabled, false)
-  shareable_link_enabled    = try(var.host.shareable_link_enabled, false)
-  kerberos_enabled          = try(var.host.kerberos_enabled, false)
-  session_recording_enabled = try(var.host.session_recording_enabled, false)
-  zones                     = try(var.host.zones, [])
-  tags                      = try(var.host.tags, var.tags, null)
-  virtual_network_id        = try(var.host.virtual_network_id, null)
+  resource_group_name = coalesce(
+    lookup(
+      var.host, "resource_group_name", null
+    ), var.resource_group_name
+  )
+
+  location = coalesce(
+    lookup(var.host, "location", null
+    ), var.location
+  )
+
+  name                      = var.host.name
+  sku                       = var.host.sku
+  scale_units               = var.host.scale_units
+  copy_paste_enabled        = var.host.copy_paste_enabled
+  file_copy_enabled         = var.host.file_copy_enabled
+  tunneling_enabled         = var.host.tunneling_enabled
+  ip_connect_enabled        = var.host.ip_connect_enabled
+  shareable_link_enabled    = var.host.shareable_link_enabled
+  kerberos_enabled          = var.host.kerberos_enabled
+  session_recording_enabled = var.host.session_recording_enabled
+  zones                     = var.host.zones
+  virtual_network_id        = var.host.virtual_network_id
+
+  tags = coalesce(
+    var.host.tags, var.tags
+  )
 
   ip_configuration {
-    name                 = try(var.host.ip_configuration.name, "configuration")
+    name                 = var.host.ip_configuration.name
     subnet_id            = var.host.ip_configuration.subnet_id
     public_ip_address_id = var.host.ip_configuration.public_ip_address_id
   }
